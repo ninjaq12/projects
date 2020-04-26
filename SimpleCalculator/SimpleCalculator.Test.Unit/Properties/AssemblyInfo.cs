85ca0811512a0896f4305b9a5a9a75d4797957e0
ninjaq12 <damian.mikolajczyk94@gmail.com>
2020-04-26 17:58:18 +0200

    Recreated SimpleCalculator with taking .gitignore into account. No more temp files in repository.

---
+++ SimpleCalculator/SimpleCalculator.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleCalculator.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleCalculator.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d9fcd1bc-fe50-4e89-a34d-a216562f3288")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

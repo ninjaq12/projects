f7b18608beb77c6c8594ebded16dd3a68b6b2ebe
ninjaq12 <damian.mikolajczyk94@gmail.com>
2020-04-26 16:05:57 +0200

    Created working SimpleCalculator. Four operations supported: addition, subtraction, multiplication, division. Also created unit test for CalculatorEngine addition.

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -10,6 +10,24 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
     }
 }

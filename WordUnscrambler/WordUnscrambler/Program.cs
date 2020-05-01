8001955672c2f488b342351d061d880b7b8fdef4
ninjaq12 <damian.mikolajczyk94@gmail.com>
2020-05-01 20:46:47 +0200

    Created skeleton of WordUnscrambler.

--- WordUnscrambler/WordUnscrambler/Program.cs
+++ WordUnscrambler/WordUnscrambler/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WordUnscrambler
 {
@@ -10,6 +11,54 @@ namespace WordUnscrambler
     {
         static void Main(string[] args)
         {
+            bool continueWordUnscramble = true;
+            do
+            {
+
+                Console.WriteLine("Please enter the option - F for File and M for Manual");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.Write("Enter scrambled words filename: ");
+                        ExecuteScrambledWordsInFileScenario();
+                        break;
+                    case "M":
+                        Console.Write("Enter scrambled words manually: ");
+                        ExecuteScrambledWordsManualEntryScenario();
+                        break;
+                    default:
+                        Console.Write("Option was not recognized.");
+                        break;
+                }
+
+                var continueWordUnscrambleDecision = string.Empty;
+                do
+                {
+
+                    Console.WriteLine("Do you want to continue? Y/N");
+                    continueWordUnscrambleDecision = (Console.ReadLine() ?? string.Empty);
+
+                } while (!continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) &&
+                         !continueWordUnscrambleDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+                continueWordUnscramble = continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase);
+
+            } while (continueWordUnscramble);
+
+
+
+        }
+
+        private static void ExecuteScrambledWordsManualEntryScenario()
+        {
+            
+        }
+
+        private static void ExecuteScrambledWordsInFileScenario()
+        {
+            
         }
     }
 }
